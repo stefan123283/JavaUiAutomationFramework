409b0f29bd789034223654aa6c294e3ce7dc3bb0
Frunza Stefan <frunzastefan356@gmail.com>
2023-11-05 17:37:12 +0200

    Implemented the PageObjects and PageFactory design patterns

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,27 +1,77 @@
 package com.opencart;
 
 import com.opencart.managers.DriverManager;
+import com.opencart.managers.RandomDataManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WindowType;
+import org.openqa.selenium.WebElement;
+
 
 public class TestRunner {
     public static void main(String[] args) throws InterruptedException {
+
         WebDriver driver = DriverManager.getInstance().getDriver();
 
-        String currentWindowName = driver.getWindowHandle();
+        try {
+            driver.get("https://www.andreisecuqa.host/");
 
-        //New Window Code
-        driver.switchTo().newWindow(WindowType.TAB);
-        driver.get("https://www.google.com/");
-        System.out.println(driver.getTitle());
-        driver.close();
+            WebElement myAccountIcon = driver.findElement(By.xpath("//i[@class='fa-solid fa-user']"));
+            myAccountIcon.click();
 
-        driver.switchTo().window(currentWindowName);
-        driver.get("https://tekwill.md/");
-        System.out.println(driver.getTitle());
-        driver.quit();
+            WebElement registerOption = driver.findElement(By.xpath("//a[normalize-space()='Register']"));
+            registerOption.click();
 
-        System.out.println("The execution is over");
+            System.out.println(driver.getCurrentUrl());
+
+            WebElement firstNameInput = driver.findElement(By.id("input-firstname"));
+
+            String firstName = RandomDataManager.generateFirstName();
+
+            firstNameInput.sendKeys(firstName);
+
+            System.out.println(firstName);
+
+            WebElement lastNameInput = driver.findElement(By.cssSelector("#input-lastname"));
+
+            String lastName = RandomDataManager.generateLastName();
+
+            lastNameInput.sendKeys(lastName);
+
+            System.out.println(lastName);
+
+            WebElement emailInput = driver.findElement(By.cssSelector("#input-email"));
+
+            String randomEmail = RandomDataManager.generateRandomEmail();
+
+            emailInput.sendKeys(randomEmail);
+
+            System.out.println(randomEmail);
+
+            WebElement passwordInput = driver.findElement(By.cssSelector("#input-password"));
+
+            String password = RandomDataManager.generatePassword();
+
+            passwordInput.sendKeys(password);
+
+            System.out.println(password);
+
+            Thread.sleep(500);
+            WebElement termsAndConditionsToggleBar = driver.findElement(By.cssSelector("input[value='1'][name='agree']"));
+
+            termsAndConditionsToggleBar.click();
+
+            WebElement registerBtn = driver.findElement(By.xpath("//button[@type='submit']"));
+
+            registerBtn.click();
+
+            System.out.println(driver.getTitle());
+
+        } catch (NoSuchElementException e) {
+            driver.close();
+            driver.quit();
+            System.out.println("The execution is over");
+        }
 
     }
 }
